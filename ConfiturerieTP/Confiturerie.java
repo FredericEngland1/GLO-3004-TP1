b05b04fd6ed4eb0e3c71db29232949e64c24f0e4
Brandon Bourgault-Morin <dieusaistout@outlook.com>
2021-04-01 19:05:57 -0400

    Suppression du BocalPool, recreation du squelette de la confiturerie et ajout des methodes de base

--- ConfiturerieTP/Confiturerie.java
+++ ConfiturerieTP/Confiturerie.java
@@ -1,42 +1,85 @@
+import org.jetbrains.annotations.Contract;
+
 public class Confiturerie {
-	
-	public static InterfaceUtilisateur UI;
-	static BocalPool _bocalPool;
-	
-	boolean _pause = false;
-	
-	int _nbBocaux = 0;
-	int _nbValves = 0;
-	int _nbEtiqueteuse = 0;
-	int _tempsSommeil = 0;
-	
-	ControleRemplissage _cRemplissage;
-	ControleEtiquetage _cEtiquetage;
-	
-	public Confiturerie (int nbBocaux, int nbValves, int nbEtiqueteuse, int tempsSommeil, InterfaceUtilisateur interfaceUtilisateur) {
-		
-		this._nbBocaux = nbBocaux;
-		this._nbValves = nbValves;
-		this._nbEtiqueteuse = nbEtiqueteuse;
-		this._tempsSommeil = tempsSommeil;
-		Confiturerie.UI = interfaceUtilisateur;
-		
-		this._cRemplissage = new ControleRemplissage(_nbValves);
-		this._cEtiquetage = new ControleEtiquetage(_nbEtiqueteuse);
-		Confiturerie._bocalPool = new BocalPool(3);
-		
-		Thread tr = new Thread(() -> _cRemplissage.Run()); // Changer le 100 par tempsSommeil
-		tr.start();
-	    
-		Thread te = new Thread(() -> _cEtiquetage.Run()); // Changer le 100 par tempsSommeil
-		te.start();
-		
+
+	private static int _nbrBocal = 0;
+	private static int _nbrValve = 0;
+	private static int _nbrEtiquette = 0;
+
+	private static int _tempsSommeil = 0;
+
+	private static boolean _pause = true;
+	private static boolean _arret = true;
+
+	private static InterfaceUtilisateur _ui;
+
+	private static ControleRemplissage _cRemplissage;
+	private static ControleEtiquetage _cEtiquetage;
+
+	public static void InitConfiturerie(int nbrBocal, int nbrValve, int nbrEtiquette, int tempsSommeil, InterfaceUtilisateur ui) {
+		_nbrBocal = nbrBocal;
+		_nbrValve = nbrValve;
+		_nbrEtiquette = nbrEtiquette;
+
+		_tempsSommeil = tempsSommeil;
+
+		_pause = false;
+		_arret = false;
+
+		_ui = ui;
+
+		//TODO add the controllers
+		_cRemplissage = ;
+		_cEtiquetage = ;
+
+		//TODO start running bocals
 	}
-	
-	public static synchronized void BocalPret (Bocal bocal, TypeBocal type) {
-		
-		_bocalPool.Release(bocal, type);
-		_bocalPool.Aquire(type);
-		
+
+	public static void ArretConfiturerie() {
+		_arret = true;
+	}
+
+	public static void MiseAJourTempsSommeil(int tempsSommeil) {
+		_tempsSommeil = tempsSommeil;
+	}
+
+	public static void Rupture(TypeBocal type) {
+		//TODO implement
+	}
+
+	public static boolean EstPause() {
+		return _pause;
+	}
+
+	public static boolean EstArret() {
+		return _arret;
+	}
+
+	public static void Pause() {
+		_pause = true;
+	}
+
+	public static void Redemarre() {
+		_pause = false;
+	}
+
+	public static ControleRemplissage GetCRemplissage() {
+		return _cRemplissage;
+	}
+
+	public static ControleEtiquetage GetCEtiquetage() {
+		return _cEtiquetage;
+	}
+
+	public static int GetTempsSommeil() {
+		return _tempsSommeil;
+	}
+
+	public static void BocalPret(Bocal bocal) {
+		//TODO implement
+	}
+
+	public static synchronized void AjouterTexte(String ligne) {
+		_ui.AjouterTexte(ligne);
 	}
 }
